32ae5e1e29ab5234f2bd81e0ceb3c60544d53727
Daniel Lee <daniel.lee@circleci.com>
2022-12-20 21:13:45 -0800

    component for upload

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,24 +1,40 @@
 import {useRef} from "react";
 
 function Upload() {
-  const ref = useRef();
+    const ref = useRef();
 
-  const handleClick = (e) => {
-    ref.current.click();
-  };
+    const handleClick = () => {
+        ref.current.click();
+    };
 
-  return (
-    <div>
-      <button
-        type="button"
-        onClick={handleClick}
-        className="bg-blue-600 px-6 py-2.5 text-white font-medium uppercase shadow-md hover:bg-blue-700 hover:shadow-lg"
-      >
-        Upload
-      </button>
-      <input ref={ref} type="file" className="hidden"></input>
-    </div>
-  );
+    const handleSubmit = async (e) => {
+        let endpoint = "http://localhost:3000/";
+        let response = await fetch(endpoint, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: e.target.files[0],
+        });
+
+        console.log(response.json());
+    };
+
+    return (
+        <div>
+            <button
+                type="button"
+                onClick={handleClick}
+                className="bg-blue-600 px-6 py-2.5 text-white text-sm font-medium uppercase shadow-md hover:bg-blue-700 hover:shadow-lg"
+            >
+                Upload
+            </button>
+            <input
+                ref={ref}
+                type="file"
+                className="hidden"
+                onChange={handleSubmit}
+            ></input>
+        </div>
+    );
 }
 
 export default Upload;

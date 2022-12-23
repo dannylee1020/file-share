ea37b019ca80c36b94efae8c7012d7cf91a68745
Daniel Lee <daniel.lee@circleci.com>
2022-12-22 19:19:13 -0800

    add route to upload file to the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,13 @@
 import express from "express";
-import {createClient} from "@supabase/supabase-js";
-import dotenv from "dotenv";
-import path from "path";
+import {upload_file} from "../controller/upload_controller.js";
 
 var router = express.Router();
-let dirname = new URL("../.env", import.meta.url).pathname;
-dotenv.config({path: dirname});
 
-const supabase = createClient(process.env.SB_URL, process.env.SB_KEY);
-
-/* GET home page. */
 router.post("/", async (req, res) => {
-    let file = req.files[0];
-    let {data, error} = await supabase.storage
-        .from("file-store")
-        .upload("test_folder/test.txt", file);
+    let file = req.files;
+    const {data, error} = await upload_file(file);
 
-    if (!error) {
-        res.send({message: "upload successful"});
-    } else {
-        res.send(error);
-    }
+    res.send({data, error});
 });
 
 export default router;

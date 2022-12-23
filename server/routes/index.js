45f7fdebb236c59383a35b058652db8e70a17d64
Daniel Lee <daniel.lee@circleci.com>
2022-12-22 20:44:12 -0800

    add new route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,19 @@
 import express from "express";
-import {upload_file} from "../controller/upload_controller.js";
+import {upload_file, retrieve_files} from "../controller/file_controller.js";
 
 var router = express.Router();
 
+// upload file to server
 router.post("/", async (req, res) => {
     let file = req.files;
     const {data, error} = await upload_file(file);
+
+    res.send({data, error});
+});
+
+// get all files in a bucket
+router.get("/files", async (req, res) => {
+    const {data, error} = await retrieve_files();
 
     res.send({data, error});
 });

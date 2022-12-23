3aeee44152079e75ad71e5cc2eab78f1d1e4a1d7
Daniel Lee <daniel.lee@circleci.com>
2022-12-22 19:17:19 -0800

    store file to server

--- server/controller/upload_controller.js
+++ server/controller/upload_controller.js
@@ -5,5 +5,14 @@ import path from "path";
 const __dirname = path.resolve();
 dotenv.config({path: path.join(__dirname, "../.env")});
 
-const supabase = createClient(process.env.SB_URL, process.env.SB_KEY);
-const {data, error} = await supabase.storage.getBucket("file-store");
+const upload_file = async function (file) {
+    const supabase = createClient(process.env.SB_URL, process.env.SB_KEY);
+
+    const {data, error} = await supabase.storage
+        .from("file-store")
+        .upload("test_folder/test.txt", file);
+
+    return {data, error};
+};
+
+export {upload_file};

ac57f5f5933dbd3f8ab126bc48a2ae679b774e34
Daniel Lee <daniel.lee@circleci.com>
2022-12-22 20:44:57 -0800

    func to fetch data from endpoint and pass it down to child

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 import Upload from "./components/Upload";
 import Files from "./components/Files";
+import {useEffect} from "react";
 
 function App() {
+    const fetch_files = async function () {
+        let endpoint = "http://localhost:3000/files";
+        let {data, error} = await fetch(endpoint, {
+            method: "GET",
+            headers: {"Content-Type": "multipart/form-data"},
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+
+        return {data, error};
+    };
+
     return (
         <div className="w-screen">
             <div className="flex flex-col gap-20 ml-10 mt-20">
                 <Upload></Upload>
-                <Files></Files>
+                <Files fetchFiles={fetch_files}></Files>
             </div>
         </div>
     );

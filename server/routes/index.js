d26b58edcdda5d6d018428ad34c68cdef237c111
Daniel Lee <daniel.lee@circleci.com>
2022-12-24 20:47:09 -0800

    adding multer to handle form data

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
 import express from "express";
+import multer from "multer";
 import {upload_file, retrieve_files} from "../controller/file_controller.js";
 
 var router = express.Router();
+const upload = multer({dest: "dest/"});
 
 // upload file to server
-router.post("/", async (req, res) => {
-    let file = req.files;
-    const {data, error} = await upload_file(file);
-
+router.post("/", upload.single("file"), async function (req, res) {
+    const {data, error} = await upload_file(req.file, req.file.originalname);
     res.send({data, error});
 });
 

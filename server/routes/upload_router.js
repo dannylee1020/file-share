5440300b09949db6fb9ca7fec203ae3fd70bc9a2
Daniel Lee <daniel.lee@circleci.com>
2022-12-24 20:50:42 -0800

    delete unused files

--- server/routes/upload_router.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-let router = express.Router();
-
-router.get("/", (req, res) => {
-    res.status(200).send("response successful!");
-});
-
-module.exports = router;

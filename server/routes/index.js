915e59e7ec8fe0cd75efe934ff9288835600cc26
Daniel Lee <daniel.lee@circleci.com>
2022-12-25 15:46:37 -0800

    adding endpoint for file donwload

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 import multer from "multer";
-import {upload_file, retrieve_files} from "../controller/file_controller.js";
+import {
+    upload_file,
+    retrieve_files,
+    download_file,
+} from "../controller/file_controller.js";
 
 var router = express.Router();
 const upload = multer({dest: "dest/"});
 
 // upload file to server
-router.post("/", upload.single("file"), async function (req, res) {
+router.post("/upload", upload.single("file"), async function (req, res) {
     const {data, error} = await upload_file(req.file, req.file.originalname);
+    res.send({data, error});
+});
+
+// download file from server
+router.post("/download", async function (req, res) {
+    let filename = req.body.filename;
+    const {data, error} = await download_file(filename);
+
     res.send({data, error});
 });
 

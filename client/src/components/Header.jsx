914f6334ef0773e6f420c46870c647ad3f8881ca
Daniel Lee <daniel.lee@circleci.com>
2022-12-28 12:08:44 +0900

    button rounder

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     return (
         <div className="border-b flex items-center justify-between mr-10">
             <div className="text-xl bold ml-10 p-2">FileShare</div>
-            <button className="bg-blue-600 text-white font-small px-2 py-1 text-sm uppercase hover:bg-blue-700 hover:shadow-lg">
+            <button className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg">
                 Log in
             </button>
         </div>

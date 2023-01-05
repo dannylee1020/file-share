feb8c0cf09f501a24ae779b76cfe36a3b26b62d7
Daniel Lee <daniel.lee@circleci.com>
2023-01-05 13:15:02 +0900

    sending binary data to client as response

--- server/controller/file_controller.js
+++ server/controller/file_controller.js
@@ -9,34 +9,30 @@ const upload_file = async function (file, name) {
     return {data, error};
 };
 
-const download_file = async function (name) {
+const download_file = async function (name, res) {
     const {data, error} = await supabase.storage
         .from("file-store")
         .download(`test_folder/${name}`);
 
-    const filePath = "/Users/dannylee/Downloads/" + name;
-
     if (error) {
-        return error;
-    } else {
-        const blob = data;
-        const buffer = Buffer.from(await blob.arrayBuffer());
-
-        await fs.promises.writeFile(filePath, buffer);
-
-        return `file saved to ${filePath}`;
+        throw error;
     }
+
+    const buffer = await new ArrayBuffer(data);
+
+    // const blob = data;
+    // const buffer = Buffer.from(await blob.arrayBuffer());
+    // await fs.promises.writeFile(filePath, buffer);
+
+    return buffer;
 };
 
 const retrieve_files = async function () {
-    // TODO: filter emptyFolderPlaceholder
     const {data, error} = await supabase.storage
         .from("file-store")
         .list("test_folder", {
             sortBy: {column: "created_at"},
         });
-
-    console.log(data);
 
     const newData = data.filter(
         (obj) => obj.name !== ".emptyFolderPlaceholder"

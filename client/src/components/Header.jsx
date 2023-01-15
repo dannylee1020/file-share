8d36456c1b912d9d892552e0b0c0602a0030595f
Daniel Lee <daniel.lee@circleci.com>
2023-01-15 14:37:10 +0900

    add navigation to ogin page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,19 @@
-// TODO: add authentication functionality
+import {useNavigate} from "react-router-dom";
 
 function Header() {
-    const handleAuth = () => {
-        // * fetch data from the server endpoint that handles authentication
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/login");
     };
 
     return (
         <div className="border-b flex items-center justify-between mr-10">
             <div className="text-xl bold ml-10 p-2">FileShare</div>
-            <button className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg">
+            <button
+                onClick={handleClick}
+                className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg"
+            >
                 Log in
             </button>
         </div>

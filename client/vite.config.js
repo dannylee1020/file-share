e4083938e8b5375853bdb943dfe87dc207591e30
Daniel Lee <daniel.lee@circleci.com>
2023-01-15 12:28:06 +0900

    add host to vite app

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import mkcert from "vite-plugin-mkcert";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    server: {https: true},
+    server: {
+        https: true,
+        host: "0.0.0.0",
+        port: 5173,
+    },
     plugins: [react(), mkcert()],
 });

2e9a9cf13e6bf25d4e0943cf8fb8e94e61e6fa1c
Daniel Lee <daniel.lee@circleci.com>
2023-01-16 15:08:33 +0900

    send request to server to register user from frontend

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,13 +1,46 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
 function Register() {
+    const [data, setData] = useState({});
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let endpoint = "http://localhost:3000/register";
+
+        await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            if (res.status === 200) {
+                navigate("/");
+            } else {
+                throw new Error(res);
+            }
+        });
+    };
+
     return (
         <div className="flex justify-center mt-20">
-            <div class="block p-6 rounded-lg shadow-lg bg-white w-1/3">
-                <form>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div class="form-group mb-6">
+            <div className="block p-6 rounded-lg shadow-lg bg-white w-1/3">
+                <form onSubmit={handleSubmit}>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="form-group mb-6">
                             <input
                                 type="text"
-                                class="form-control
+                                className="form-control
                                     block
                                     w-full
                                     px-3
@@ -24,13 +57,15 @@ function Register() {
                                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 id="exampleInput123"
                                 aria-describedby="emailHelp123"
+                                name="firstname"
+                                onChange={handleChange}
                                 placeholder="First name"
                             />
                         </div>
-                        <div class="form-group mb-6">
+                        <div className="form-group mb-6">
                             <input
                                 type="text"
-                                class="form-control
+                                className="form-control
                                     block
                                     w-full
                                     px-3
@@ -47,14 +82,16 @@ function Register() {
                                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 id="exampleInput124"
                                 aria-describedby="emailHelp124"
+                                name="lastname"
+                                onChange={handleChange}
                                 placeholder="Last name"
                             />
                         </div>
                     </div>
-                    <div class="form-group mb-6">
+                    <div className="form-group mb-6">
                         <input
                             type="email"
-                            class="form-control block
+                            className="form-control block
                                 w-full
                                 px-3
                                 py-1.5
@@ -69,13 +106,15 @@ function Register() {
                                 m-0
                                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleInput125"
+                            name="email"
+                            onChange={handleChange}
                             placeholder="Email address"
                         />
                     </div>
-                    <div class="form-group mb-6">
+                    <div className="form-group mb-6">
                         <input
                             type="password"
-                            class="form-control block
+                            className="form-control block
                                 w-full
                                 px-3
                                 py-1.5
@@ -90,26 +129,30 @@ function Register() {
                                 m-0
                                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleInput126"
+                            name="password"
+                            onChange={handleChange}
                             placeholder="Password"
                         />
                     </div>
-                    <div class="form-group form-check text-center mb-6">
+                    <div className="form-group form-check text-center mb-6">
                         <input
                             type="checkbox"
-                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
                             id="exampleCheck25"
+                            name="checkbox"
+                            onChange={handleChange}
                             checked
                         />
                         <label
-                            class="form-check-label inline-block text-gray-800"
-                            for="exampleCheck25"
+                            className="form-check-label inline-block text-gray-800"
+                            htmlFor="exampleCheck25"
                         >
                             Subscribe to our newsletter
                         </label>
                     </div>
                     <button
                         type="submit"
-                        class="w-full
+                        className="w-full
                             px-6
                             py-2.5
                             bg-blue-600

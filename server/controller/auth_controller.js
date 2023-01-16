5f12dffe2b1951798355cac88d28b2dc11a09e2e
Daniel Lee <daniel.lee@circleci.com>
2023-01-16 15:08:53 +0900

    make api call to supabase for registration

--- server/controller/auth_controller.js
+++ server/controller/auth_controller.js
@@ -1,7 +1,25 @@
 import supabase from "../config.js";
+import {body} from "express-validator";
 
 const auth_user = async () => {
     // call supabase api for authentication
 };
 
-export {auth_user};
+const validate_register = () => {};
+
+const register_user = async (body) => {
+    const {data, error} = await supabase.auth.signUp({
+        email: body.email,
+        password: body.password,
+        options: {
+            data: {
+                firstname: body.firstname,
+                lastname: body.lastname,
+            },
+        },
+    });
+
+    return {data, error};
+};
+
+export {auth_user, register_user};

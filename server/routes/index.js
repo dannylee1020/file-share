a9a12a5f79ad669952815339b1c046672f8a6ccc
Daniel Lee <daniel.lee@circleci.com>
2023-01-16 15:09:04 +0900

    add new route for registration

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import fs from "fs";
+import {body, validationResult} from "express-validator";
 
 import {
     upload_file,
@@ -8,7 +9,7 @@ import {
     download_file,
 } from "../controller/file_controller.js";
 
-import {auth_user} from "../controller/auth_controller.js";
+import {register_user} from "../controller/auth_controller.js";
 
 var router = express.Router();
 const upload = multer({dest: "dest/"});
@@ -39,5 +40,29 @@ router.get("/files", async (req, res) => {
 router.get("/auth", (req, res) => {
     // handle authentication here
 });
+
+// register users to the app
+router.post(
+    "/register",
+
+    body("firstname").trim().escape(),
+    body("lastname").trim().escape(),
+    body("password").trim().escape(),
+    body("email").isEmail().normalizeEmail({all_lowercase: true}),
+
+    (req, res) => {
+        let result = validationResult(req);
+
+        if (!result.isEmpty()) {
+            return res.json({errors: result.array()});
+        }
+
+        let {data, error} = register_user(req.body);
+
+        error
+            ? res.status(404).send(error)
+            : res.status(200).json({message: "OK"});
+    }
+);
 
 export default router;

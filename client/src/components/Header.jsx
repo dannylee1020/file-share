4a80c2c3744070954214ceebf1996bf52e1dee7c
Daniel Lee <daniel.lee@circleci.com>
2023-01-20 15:34:12 +0900

    adding handler for signing user out and conditionally rendering button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,19 +3,42 @@ import {useNavigate} from "react-router-dom";
 function Header() {
     const navigate = useNavigate();
 
-    const handleClick = () => {
+    const handleLogin = () => {
         navigate("/login");
+    };
+
+    const handleLogout = async () => {
+        let endpoint = "http://localhost:3000/logout";
+
+        await fetch(endpoint, {
+            method: "GET",
+        })
+            .then((res) => res.json())
+            .then(() => {
+                localStorage.removeItem("session");
+                window.location.reload();
+            });
     };
 
     return (
         <div className="border-b flex items-center justify-between mr-10">
             <div className="text-xl bold ml-10 p-2">FileShare</div>
-            <button
-                onClick={handleClick}
-                className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg"
-            >
-                Log in
-            </button>
+
+            {localStorage.getItem("session") ? (
+                <button
+                    onClick={handleLogout}
+                    className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg"
+                >
+                    Log out
+                </button>
+            ) : (
+                <button
+                    onClick={handleLogin}
+                    className="bg-blue-600 text-white font-small px-2 py-1 text-sm rounded uppercase hover:bg-blue-700 hover:shadow-lg"
+                >
+                    Log in
+                </button>
+            )}
         </div>
     );
 }

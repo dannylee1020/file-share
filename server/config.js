e4044fff865c033ae253b6378a68a95090c9bb11
Jay Heo <heojay0309@gmail.com>
2023-03-03 16:38:43 -0500

    :art: testing

--- server/config.js
+++ server/config.js
@@ -4,6 +4,7 @@ import {createClient} from "@supabase/supabase-js";
 var envPath = new URL(".env", import.meta.url).pathname;
 dotenv.config({path: envPath});
 
-const supabase = createClient(process.env.SB_URL, process.env.SB_KEY);
+const supabase = createClient('https://iyuctluisbrgxeykwvay.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dWN0bHVpc2JyZ3hleWt3dmF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0MDQwNzEsImV4cCI6MTk4Njk4MDA3MX0.BKBgdPKQvKwajuf_6chK2YLc0qkFtuCn8GLrptLgR0I');
+// const supabase = createClient(process.env.SB_URL, process.env.SB_KEY);
 
 export default supabase;
